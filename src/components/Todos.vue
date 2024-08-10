7d2a002e8887a2123f9589af73bdf474d23a53f3
Carl Schroedl <carlschroedl@gmail.com>
2024-08-10 10:06:21 -0500

    Completed storage setup https://docs.amplify.aws/vue/build-a-backend/storage/set-up-storage/

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -29,15 +29,47 @@ function createTodo() {
 function deleteTodo(id: string) {
   client.models.Todo.delete({ id })
 }
+
+import { uploadData } from "aws-amplify/storage";
+function initUpload() {
+  const file = document.getElementById("file") as HTMLInputElement;
+  const upload = document.getElementById("upload") as HTMLElement;
+
+  upload.addEventListener("click", () => {
+    const fileReader = new FileReader();
+    // @ts-ignore
+    fileReader.readAsArrayBuffer(file.files[0]);
+
+    fileReader.onload = async (event) => {
+      // @ts-ignore
+      console.log("Complete File read successfully!", event.target.result);
+      try {
+        await uploadData({
+          // @ts-ignore
+          data: event?.target?.result,
+          // @ts-ignore
+          path: `picture-submissions/${file.files[0].name}`
+        });
+      } catch (e) {
+        console.log("error", e);
+      }
+    };
+  });
+}
+
 // fetch todos when the component is mounted
  onMounted(() => {
   listTodos();
+  initUpload();
 });
 
 </script>
 
 <template>
   <main>
+    <h1>My pictures</h1>
+    <input type="file" id="file"/>
+    <button id="upload">Upload</button>
     <h1>My todos</h1>
     <button @click="createTodo">+ new</button>
     <ul>
